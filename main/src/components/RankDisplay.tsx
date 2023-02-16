e5df0f80b09d0b6c1d91b093ea948b25a322d98d
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-02-16 14:01:30 +0100

    Changed design layout for rankdisplay

--- main/src/components/RankDisplay.tsx
+++ main/src/components/RankDisplay.tsx
@@ -4,22 +4,23 @@ import { useEffect, useState } from "react";
 const RankDisplay = () => {
 
     const [summonerName, setSummonerName] = useState("Extension is not configured!");
-    const [divison, setDivision] = useState("");
-    const [tier, setTier] = useState ("");
+    const [divison, setDivision] = useState("Bronze");
+    const [tier, setTier] = useState ("II");
 
     useEffect(() => {    
         window.Twitch.ext.onAuthorized((auth : any) => {
             var config = JSON.parse(window.Twitch.ext.configuration.broadcaster.content);
             setSummonerName(config[0])
-
         });
     }, []);
 
     return (
         <div id="rankDisplay">
             <img id="divisionImage" src={divisions.Bronze} alt="league rank"/>
-            <h1 id="summonerName">{summonerName}</h1>
-            <h1 id="divisonTierTitle">{divison} - {tier}</h1>
+            <div id="rankDisplayFlexItem">
+                <h1 id="summonerName">{summonerName}</h1>
+                <h1 id="divisonTierTitle">{divison} - {tier}</h1>
+            </div>
         </div>
     )
 }
